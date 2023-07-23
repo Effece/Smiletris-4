09a7a954e0f662649b15d295ffb5c0185819478d
Effece <79398531+Effece@users.noreply.github.com>
2023-07-23 10:09:12 +0200

    Updating to v0.1
    Fixed a lot of bugs, added a few new features, added images, documented everything.

--- elements/Smiley.java
+++ elements/Smiley.java
@@ -1,3 +1,8 @@
+/*
+ * Smiley to destroy to win the game. They are generated when the level is created.
+ * They are made from the cell's to the only difference that they can't fall.
+ */
+
 package elements;
 
 import game.Grid;
@@ -6,11 +11,15 @@ public class Smiley extends Cell {
 
 	public Smiley(Grid grid, int x, int y, int color) {
 		
+		// set to a cell that isn't fused
 		super(grid, x, y, false, false, null, color);
 		
 	}
 	
 	public boolean canFall() {
+		/*
+		 * A smiley never falls.
+		 */
 		
 		return false;
 		

519ea620d04d9c5a3751d5a390e907d38aec7a85
Effece <79398531+Effece@users.noreply.github.com>
2023-07-23 10:11:20 +0200

    Updating to v0.1
    Fixed a lot of bugs, added a few new features, added images, documented everything.

--- visuals/Frame.java
+++ visuals/Frame.java
@@ -1,41 +1,44 @@
+/*
+ * Window in which the game happens. Contains the canvas.
+ * 
+ * The window is mesured in DPI while the canvas is mesured in pixels.
+ * 
+ * Libraries:
+ *  java.awt
+ */
+
 package visuals;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.BorderLayout;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 import game.Game;
 
-public class Frame extends JFrame {
+public class Frame extends java.awt.Frame implements WindowListener {
 	
 	Game game;
 	
-	final JPanel canPanel;
-	public final int width, height, rap;
+	public final int width, height,
+					 rap; // size of a case
 	Color background;
 	
 	private static final long serialVersionUID = 4L;
-
+	
 	public Frame(Game game) {
+		
+		super();
 		
 		this.game = game;
 		
-		this.rap = 30;
+		this.rap = 32;
 		this.background = Color.black;
 		
 		this.setBackground(this.background);
-		
-		this.setVisible(true);
-		
-		this.canPanel = new JPanel();
-		this.add(this.canPanel, BorderLayout.CENTER);
-		this.canPanel.setBackground(this.background);
-		this.canPanel.setVisible(true);
 	    
-	    this.width = this.game.grid.width * this.rap;
-		this.height = this.game.grid.height * this.rap;
+	    this.width = 1920;  // maximum size \ it doesn't matter because the canvas runs
+		this.height = 1280; // maximum size / a function to make it full-screen
 		
 		this.setSize(this.width, this.height);
 	    this.setPreferredSize(new Dimension(this.width, this.height));
@@ -44,10 +47,42 @@ public class Frame extends JFrame {
 	    
 	    this.setTitle("Smiletris 4");
 	    this.setFocusable(true);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		this.addWindowListener(this);
+		
+		this.setVisible(true);
 		this.pack();
 		
 	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		/*
+		 * End of the program.
+		 */
+		
+		this.dispose();
+		this.game.onGoing = false;
+		System.exit(0);
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {}
+
+	@Override
+	public void windowIconified(WindowEvent e) {}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {}
+
+	@Override
+	public void windowActivated(WindowEvent e) {}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {}
 	
 }
